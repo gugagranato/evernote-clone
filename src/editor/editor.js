0ba661290a0f1fedaa5f1f85ba841009a8571e10
Gustavo Granato Silva <gustavo.granato@hotmail.com>
2019-07-16 01:21:36 -0300

    created components editor, sidebar and sidebaritem

---
+++ src/editor/editor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+class EditorComponent extends Component {
+    constructor() {
+        super();
+    }
+  
+    render() {
+    return (
+        <div>Hello from Editor Component</div> 
+    );
+  }
+}
+
+export default withStyles(styles)(EditorComponent);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const firebase = require('firebase');
+require('firebase/firestore');
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCX3bXnoUe9GrWw7bPoqitLyE8sbxzyihc",
+    authDomain: "evernote-clone-7151d.firebaseapp.com",
+    databaseURL: "https://evernote-clone-7151d.firebaseio.com",
+    projectId: "evernote-clone-7151d",
+    storageBucket: "evernote-clone-7151d.appspot.com",
+    messagingSenderId: "130372742057",
+    appId: "1:130372742057:web:25d0e2b2ff56d19c"
+  });
+
+ReactDOM.render(<App />, document.getElementById('evernote-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
