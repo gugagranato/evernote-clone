22a12411f2e2af38bb6d923fcd2c47e375af6b20
Gustavo Granato Silva <gustavo.granato@hotmail.com>
2019-07-18 13:52:01 -0300

    update database

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -8,13 +8,35 @@ import styles from './styles';
 class EditorComponent extends Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '', 
+            id: ''
+        }
     }
   
     render() {
+
+        const { classes } = this.props;
+
     return (
-        <div>Hello from Editor Component</div> 
+        <div className={classes.editorContainer}>
+            <ReactQuill 
+                value={this.state.text}
+                onChange={this.updateBody}>
+            </ReactQuill>
+        </div> 
     );
   }
+
+  updateBody = async (val) => {
+    await this.setState({ text: val });
+    this.update();
+  };
+
+  update = debounce(() => {
+    console.log('update database')
+    }, 1500);
 }
 
 export default withStyles(styles)(EditorComponent);
